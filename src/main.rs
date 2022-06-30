1efa368753d54f9d56d2d3c5ff7e3992e2f5fd3b
revandrazm <mahartarevandra@gmail.com>
2022-06-30 22:25:39 +0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+mod theme;
+
+use std::error::Error;
+use dotenv::dotenv;
+
+use newsapi::{get_articles, Articles};
+
+fn render_articles(articles: &Articles) {
+    let theme = theme::default();
+    theme.print_text("# Top Headlines\n\n");
+
+    for article in &articles.articles {
+        theme.print_text(&format!("`{}`", article.title));
+        theme.print_text(&format!("> *{}*", article.url));
+        theme.print_text("---");
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    dotenv();
+
+    let api_key = std::env::var("API_KEY")?;
+
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=";
+    let url = format!("{}{}", url, api_key);
+
+    let articles = get_articles(&url)?;
+
+    render_articles(&articles);
+
+    Ok(())
+}
